a5d922cdb58844ece2cb2f9c348ebf1da57b4b75
RaidaAsh <raidanahian@gmail.com>
2018-06-26 17:38:49 +0600

    GRAMI 0.0.2

--- grami.cpp
+++ grami.cpp
@@ -279,7 +279,52 @@ Subgraphs extendBackwardEdge(Subgraphs currSubgraph, int u, int v, int edgeLabel
 	return newSubgraph;
 }
 
-CSP domainAfterForwardExtension(int uLabel, int vLabel, int edgeLabel, CSP csp){
+CSP domainAfterBackwardExtension(int u, int uLabel, int v, int vLabel, int edgeLabel, CSP csp){
+	CSP newCsp=csp;
+	int sz=newCsp.domain[u].size();
+	int forwardIterator=-1, val;
+	for(int i=0; i<sz; i++){
+		val=newCsp.domain[u][i];
+		if(outgoingLabels[val].find(pii(vLabel, edgeLabel))==outgoingLabels[val].end()){ //Need to remove val from domain[u]
+			if(forwardIterator==-1){
+				forwardIterator=i;
+			}
+		}
+		else{ //Keep val in domain[u]
+			if(forwardIterator==-1) continue;
+			swap(newCsp.domain[u][i], newCsp.domain[u][forwardIterator]);
+			forwardIterator++;
+		}
+	}
+	for(int i=(forwardIterator==-1) ? sz: forwardIterator; i<sz; i++){
+		newCsp.domain[u].pop_back();
+	}
+	newCsp.mnSize=min(newCsp.mnSize, (unsigned int) newCsp.domain[u].size());
+	sz=newCsp.domain[v].size();
+	forwardIterator=-1;
+	for(int i=0; i<sz; i++){
+		val=newCsp.domain[v][i];
+		if(incomingLabels[val].find(pii(uLabel, edgeLabel))==incomingLabels[val].end()){ //Need to remove val from domain[u]
+			if(forwardIterator==-1){
+				forwardIterator=i;
+			}
+		}
+		else{ //Keep val in domain[u]
+			if(forwardIterator==-1) continue;
+			swap(newCsp.domain[v][i], newCsp.domain[v][forwardIterator]);
+			forwardIterator++;
+		}
+	}
+	for(int i=(forwardIterator==-1) ? sz: forwardIterator; i<sz; i++){
+		newCsp.domain[v].pop_back();
+	}
+	newCsp.mnSize=min(newCsp.mnSize, (unsigned int) newCsp.domain[v].size());
+	return newCsp;
+	return newCsp;
+	
+}
+
+CSP domainAfterForwardExtension(int u, int uLabel, int vLabel, int edgeLabel, CSP csp){
 	CSP newCsp=csp;
 	vector <int> domainOfExtendedNode;
 	int nodeInConsideration;
@@ -293,6 +338,26 @@ CSP domainAfterForwardExtension(int uLabel, int vLabel, int edgeLabel, CSP csp){
 	newCsp.domain.push_back(domainOfExtendedNode);
 	newCsp.mnSize=min(newCsp.mnSize,(unsigned int) domainOfExtendedNode.size());
 	domainOfExtendedNode.clear();
+	
+	sz=newCsp.domain[u].size();
+	int forwardIterator=-1, val;
+	for(int i=0; i<sz; i++){
+		val=newCsp.domain[u][i];
+		if(outgoingLabels[val].find(pii(vLabel, edgeLabel))==outgoingLabels[val].end()){ //Need to remove val from domain[u]
+			if(forwardIterator==-1){
+				forwardIterator=i;
+			}
+		}
+		else{ //Keep val in domain[u]
+			if(forwardIterator==-1) continue;
+			swap(newCsp.domain[u][i], newCsp.domain[u][forwardIterator]);
+			forwardIterator++;
+		}
+	}
+	for(int i=(forwardIterator==-1) ? sz: forwardIterator; i<sz; i++){
+		newCsp.domain[u].pop_back();
+	}
+	newCsp.mnSize=min(newCsp.mnSize, (unsigned int) newCsp.domain[u].size());
 	return newCsp;
 }
 
@@ -365,20 +430,20 @@ inline bool randomTermination(){
 void subgraphExtension(Subgraphs currSubgraph, CSP csp){
 	globalVariable++;
 	if((globalVariable%10)==0) printf("%d\n", globalVariable);
-	printSubgraph(currSubgraph);
-	printCSP(csp);	
-	if(userWantsToTerminate())
+	//printSubgraph(currSubgraph);
+	//printCSP(csp);	
+	/*if(userWantsToTerminate())
 	{
 		printf("Terminated\n");
 		return;
-	}
+	}*/
 	
 	/*if(randomTermination()){
 		return;
 	}*/
 	
 	if(csp.mnSize<threshold){
-		printf("Terminating anyways\n");
+		//printf("Terminating anyways\n");
 		return;
 	}
 	
@@ -395,14 +460,15 @@ void subgraphExtension(Subgraphs currSubgraph, CSP csp){
 			for(int j=0; j<numberOfEdgeLabels; j++){
 				if(labelRelations.find(ppi(pii(currSubgraph.nodeLabels[currNode], currSubgraph.nodeLabels[i]), j))==labelRelations.end()) continue;
 				newSubgraph=extendBackwardEdge(currSubgraph, currNode, i, j);
-				subgraphExtension(newSubgraph,csp);
+				newCsp = domainAfterBackwardExtension(currNode,currSubgraph.nodeLabels[currNode],i,currSubgraph.nodeLabels[i],j,csp);
+				subgraphExtension(newSubgraph,newCsp);
 			}
 		}
 		for(int i=0; i<numberOfNodeLabels; i++){
 			for(int j=0; j<numberOfEdgeLabels; j++){
 				if(labelRelations.find(ppi(pii(currSubgraph.nodeLabels[currNode], i), j))==labelRelations.end()) continue;
 				newSubgraph=extendForwardEdge(currSubgraph, currNode, currSubgraph.numberOfNodes, i, j);
-				newCsp=domainAfterForwardExtension(currSubgraph.nodeLabels[currNode], i, j, csp);
+				newCsp=domainAfterForwardExtension(currNode, currSubgraph.nodeLabels[currNode], i, j, csp);
 				subgraphExtension(newSubgraph,newCsp);
 			}
 		}
